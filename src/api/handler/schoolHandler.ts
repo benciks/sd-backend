65974b7716e51644841c61ceac0c62aeb55b0117
Simon Bencik <bencikdesign@gmail.com>
2021-01-15 16:41:57 +0100

    School routes, validation

--- src/api/handler/schoolHandler.ts
+++ src/api/handler/schoolHandler.ts
@@ -6,23 +6,33 @@ import { School } from '../../db/entity/school'
 
 export default class SchoolHandler {
     async initialize() {
-        router.get('/schools', wrap(this.getSchool))
-        router.post('/schools', routeGuard(), wrap(this.postSchool))
+        router.get('/schools', wrap(this.getAllSchools))
+        router.get('/schools/:id', wrap(this.getSchool))
+        router.post('/schools', routeGuard(), wrap(this.addSchool))
+        router.patch('/schools/:id', routeGuard(), wrap(this.updateSchool))
+        router.delete('/schools/:id', routeGuard(), wrap(this.deleteSchool))
     }
 
-    async getSchool() {
+    async getAllSchools() {
         return School.find()
     }
 
-    async postSchool(req: Express.Request) {
-        const body = req.body
-        await School.insert({
-            name: body.name,
-            url: body.url,
-            address: body.address,
-            city: body.city,
-            postal: body.postal,
-            description: body.description,
-        })
+    async getSchool(req: Express.Request) {
+        return School.findOne({ where: { id: req.params.id } })
+    }
+
+    async addSchool(req: Express.Request) {
+        await School.insert(req.body)
+    }
+
+    async updateSchool(req: Express.Request) {
+        const school = await School.findOne({ where: { id: req.params.id } })
+
+        Object.assign(school, req.body)
+        await school.save()
+    }
+
+    async deleteSchool(req: Express.Request) {
+        await School.delete(req.params.id)
     }
 }
