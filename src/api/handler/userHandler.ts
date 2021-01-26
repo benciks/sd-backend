e14b214986930242b6939eac4c434fcc0601bb8b
Simon Bencik <bencikdesign@gmail.com>
2021-01-26 16:19:59 +0100

    Added user update

--- src/api/handler/userHandler.ts
+++ src/api/handler/userHandler.ts
@@ -8,6 +8,7 @@ export default class UserHandler {
     async initialize() {
         router.get('/users', routeGuard(), wrap(this.getAllUsers))
         router.get('/users/me', routeGuard(), wrap(this.getUserMe))
+        router.post('/users/me', routeGuard(), wrap(this.updateUser))
     }
 
     async getAllUsers() {
@@ -16,5 +17,20 @@ export default class UserHandler {
 
     async getUserMe(req: Express.Request) {
         return await req.getUser()
+    }
+
+    async updateUser(req: Express.Request) {
+        const body = req.body
+        const currentUser = await req.getUser()
+
+        const user = await User.findOne({ where: { id: currentUser.id } })
+
+        Object.assign(user, {
+            name: body.name,
+            email: body.email,
+            password: body.password,
+        })
+
+        await user.save()
     }
 }
