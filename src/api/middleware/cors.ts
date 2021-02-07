64c6b8af9c10546e304427455ed0560930fd8d37
Simon Bencik <bencikdesign@gmail.com>
2021-02-07 22:51:13 +0100

    Added origin to cors

--- src/api/middleware/cors.ts
+++ src/api/middleware/cors.ts
@@ -5,8 +5,8 @@ export function registerCors() {
     const corsMiddleware = cors({
         origin:
             process.env.NODE_ENV === 'production'
-                ? 'https://'
-                : ['localhost', 'localhost:3000', 'localhost:3001', 'localhost:8080'],
+                ? ['https://', 'http://localhost:8080']
+                : ['localhost', 'localhost:3000', 'localhost:3001', 'http://localhost:8080'],
         methods: ['GET', 'POST', 'DELETE'],
         allowedHeaders: ['Authorization', 'content-type'],
     })
