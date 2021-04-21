6343a715deaa1b1450d4e23ad74a3d097af8fb83
Simon Bencik <bencikdesign@gmail.com>
2021-04-21 22:04:41 +0200

    Changed cors settings

--- src/api/middleware/cors.ts
+++ src/api/middleware/cors.ts
@@ -5,7 +5,7 @@ export function registerCors() {
     const corsMiddleware = cors({
         origin:
             process.env.NODE_ENV === 'production'
-                ? ['https://', 'http://localhost:8080', 'http://simon-sd-landing.server2.trail.group/']
+                ? ['https://simon-sd-landing.server2.trail.group/']
                 : ['localhost', 'localhost:3000', 'localhost:3001', 'http://localhost:8080'],
         methods: ['GET', 'POST', 'PATCH', 'DELETE'],
         allowedHeaders: ['Authorization', 'content-type'],
